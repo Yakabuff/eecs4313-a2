ae8f2c6eb7bbefc480ab4a8092cc9848c3165e7e
Daniel K <daniel.kozlovsky@gmail.com>
2021-02-24 23:34:34 -0500

    Set up test suite in common and added hamcrest

---
+++ common/src/test/java/eecs4313a2b/EECS4313A2AllBlackBoxTests.java
@@ -0,0 +1,21 @@
+package eecs4313a2b;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import net.sf.borg.common.DateUtil;
+
+public class EECS4313A2AllBlackBoxTests
+{
+	
+	/*********************************************Method1 Tests***********************************************************/
+	@Test
+	public void test1()
+	{
+		assertEquals(true, true);
+	}
+	/*********************************************Method2 Tests***********************************************************/
+	
+	
+	/*********************************************Method3 Tests***********************************************************/
+}
---
+++ common/src/test/java/eecs4313a2w/EECS4313A2AllWhiteBoxTests.java
@@ -0,0 +1,15 @@
+package eecs4313a2w;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class EECS4313A2AllWhiteBoxTests
+{
+	@Test
+	public void test1()
+	{
+		assertEquals(true, true);
+	}
+}
